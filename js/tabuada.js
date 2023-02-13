99a4c4d077ccce13c131666f21e78dc906124812
Jéssica Moreira <100448388+JessicaMoreiraS@users.noreply.github.com>
2023-02-12 22:34:46 -0300

    Add files via upload

---
+++ js/tabuada.js
@@ -0,0 +1,28 @@
+function calcular(){
+    var tabuadaDo = parseInt(document.getElementById("tabuadaDo").value);
+    var ate = parseInt(document.getElementById("ate").value);
+    var mostrar = document.getElementById("mostrar");
+    
+    limpar()
+
+    mostrar.style.display='block'
+
+    mostrar.innerHTML += '<div class="margem"><p>.</p></div>';
+    for(i=0; i<=ate; i++){
+        const conta = "<p>"+tabuadaDo+" x "+i+" = "+tabuadaDo*i+"</p>";
+        mostrar.innerHTML +=conta;
+    }
+    if(tabuadaDo*i>1000){
+        mostrar.style.width='70%'
+    }
+    if(tabuadaDo*i<1000){
+        mostrar.style.width='45%'
+    }
+
+    mostrar.innerHTML += '<div class="margem"><p>.</p></div>';
+
+}
+function limpar(){
+    mostrar.innerHTML = ""
+    mostrar.style.display='none'
+}
